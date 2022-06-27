0b8d84debf801bb7fa1ae076f3d6570811b2d811
wardantech <wardan.tech@gmail.comgit config --global user.email wardan.tech@gmail.comgit config --global user.email wardan.tech@gmail.com>
2022-06-27 11:24:17 +0600

    discrtict issue solve

--- app/Models/Settings/District.php
+++ app/Models/Settings/District.php
@@ -9,5 +9,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class District extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 }
--- app/Models/Settings/Division.php
+++ app/Models/Settings/Division.php
@@ -8,5 +8,5 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Division extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 }
