b83382e1056fada064a1f8b88cff770e89b77de2
shahriarshaon19 <shahriarshaon1993@gmail.com>
2022-07-04 03:54:41 +0600

    Property Single Show

--- app/Models/Property/Property.php
+++ app/Models/Property/Property.php
@@ -3,6 +3,10 @@
 namespace App\Models\Property;
 
 use App\Models\Landlord;
+use App\Models\Settings\District;
+use App\Models\Settings\Division;
+use App\Models\Settings\PropertyType;
+use App\Models\Settings\Thana;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,5 +19,30 @@ class Property extends Model
     public function landloard(): BelongsTo
     {
         return $this->belongsTo(Landlord::class, 'landloard_id', 'id')->withTrashed();
+    }
+
+    public function thana()
+    {
+        return $this->belongsTo(Thana::class, 'thana_id', 'id');
+    }
+
+    public function district()
+    {
+        return $this->belongsTo(District::class, 'district_id', 'id');
+    }
+
+    public function division()
+    {
+        return $this->belongsTo(Division::class, 'division_id', 'id');
+    }
+
+    public function propertyType()
+    {
+        return $this->belongsTo(PropertyType::class, 'property_type_id', 'id');
+    }
+
+    public function landlord()
+    {
+        return $this->belongsTo(Landlord::class, 'landlord_id', 'id');
     }
 }
--- routes/admin.php
+++ routes/admin.php
@@ -178,6 +178,7 @@ Route::group(['middleware' => ['auth:api']], function(){
     // Property Route
     Route::group(['prefix' => 'property'], function() {
         Route::post('store', [PropertyController::class, 'store']);
+        Route::get('show/{id}',[PropertyController::class, 'show']);
         Route::get('get-property-type', [PropertyController::class, 'getPropertyTypes']);
         Route::post('list', [PropertyController::class,'getList']);
         Route::post('change-status/{id}',[PropertyController::class, 'changeStatus']);
