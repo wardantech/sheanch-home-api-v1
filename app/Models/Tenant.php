76c930e45409fd54158bf0456dc7f55a15cd7c71
shahriarshaon19 <shahriarshaon1993@gmail.com>
2022-08-16 18:36:53 +0600

    tenant show

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Settings\District;
+use App\Models\Settings\Division;
+use App\Models\Settings\Thana;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -15,4 +18,20 @@ class Tenant extends Model
     {
         return $this->belongsTo(User::class,'id','landlord_id')->withTrashed();
     }
+
+    public function thana()
+    {
+        return $this->belongsTo(Thana::class, 'thana_id', 'id');
+    }
+
+    public function district()
+    {
+        return $this->belongsTo(District::class, 'district_id', 'id');
+    }
+
+    public function division()
+    {
+        return $this->belongsTo(Division::class, 'division_id', 'id');
+    }
+
 }
