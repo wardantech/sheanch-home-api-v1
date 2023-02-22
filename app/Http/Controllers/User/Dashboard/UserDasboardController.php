dfee30f8b1582548843bd00202a812f5bc7da382
shahriarshaon1993 <shahriarshaon1993@gmail.com>
2023-02-22 11:19:44 +0600

    Approved Deed Lists Show In Dashboard

--- app/Http/Controllers/User/Dashboard/UserDasboardController.php
+++ app/Http/Controllers/User/Dashboard/UserDasboardController.php
@@ -16,16 +16,48 @@ class UserDasboardController extends Controller
     public function getDashboardData(Request $request)
     {
         try {
-            $totalProperties = Property::where('user_id', $request->userId)->where('status',1)->count();
-            $totalPoropertyAds = PropertyAd::where('user_id', $request->userId)->where('status',1)->count();
-            $totalCompleteDeed = PropertyDeed::where('landlord_id', $request->userId)->where('status',2)->count();
+            $columns = ['id', 'name'];
+            $length = $request['params']['length'];
+            $column = $request['params']['column'];
+            $dir = $request['params']['dir'];
+            $searchValue = $request['params']['search'];
+            $userId = $request['params']['userId'];
 
-            return $this->sendResponse([
+            $totalProperties = Property::where('user_id', $userId)->where('status',1)->count();
+            $totalPoropertyAds = PropertyAd::where('user_id', $userId)->where('status',1)->count();
+            $totalCompleteDeed = PropertyDeed::where('landlord_id', $userId)->where('status',2)->count();
+
+            $query = PropertyDeed::with(['tenant' => function($query){
+                $query->select('id', 'name');
+            }, 'property' => function($query){
+                $query->select('id', 'name');
+            }])
+            ->where('status', 5)
+            ->where('landlord_id', $userId)
+            ->orderBy($columns[$column], $dir);
+
+            $count = PropertyDeed::count();
+
+            if ($searchValue) {
+                $query->where(function ($query) use ($searchValue) {
+                    $query->where('id', 'like', '%' . $searchValue . '%')
+                        ->orWhere('name', 'like', '%' . $searchValue . '%');
+                });
+            }
+
+            if ($length != 'all') {
+                $fetchData = $query->paginate($length);
+            } else {
+                $fetchData = $query->paginate($count);
+            }
+
+            return [
+                'data' => $fetchData,
+                'draw' => $request['params']['draw'],
                 'totalProperties' => $totalProperties,
-                'totalPropertyAds' => $totalPoropertyAds,
+                'totalPoropertyAds' => $totalPoropertyAds,
                 'totalCompleteDeed' => $totalCompleteDeed,
-
-            ],'Dashboard data get successfully');
+            ];
         }catch (\Exception $exception){
             return $this->sendError('Dashboard data get error', ['error' => $exception->getMessage()]);
         }
